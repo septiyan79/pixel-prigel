da4c51c1468febeca8dd3f136d3ea7deefa95249
Septyan Eka Prastyio <4885@dnpi.co.id>
2026-01-26 16:49:42 +0700

    rapihkan Navbar Public (belum selesai)

--- src/public/components/Navbar.jsx
+++ src/public/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { RiUser5Line } from "react-icons/ri";
+import { RiUser5Line, RiMenu3Line, RiLogoutBoxRLine, RiLoginBoxFill } from "react-icons/ri";
 
 import { useAuth } from "../../auth/AuthProvider";
 import { useLogoutConfirm } from "../../hooks/useLogoutConfirm";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const navigate = useNavigate();
   const { logout } = useAuth();
 
@@ -19,7 +22,8 @@ export default function Navbar() {
     <div className="fixed top-4 inset-x-0 z-50 flex justify-center px-4">
       <nav className="bg-white/80 backdrop-blur-md border-2 border-orange-600 px-6 py-3 rounded-2xl flex items-center gap-8 shadow-[4px_4px_0px_0px_rgba(234,88,12,1)]">
         <span className="font-black text-xl tracking-tighter text-orange-600">Pixel Prigel</span>
-        <div className="hidden md:flex gap-6 text-sm font-bold uppercase tracking-wider">
+        
+        <div className={`md:flex gap-6 text-sm font-bold uppercase tracking-wider ${menuOpen ? 'block' : 'hidden'}`}>
           <NavLink
             to="/"
             className={({ isActive }) =>
@@ -65,14 +69,23 @@ export default function Navbar() {
 
         </div>
         <div className="h-6 w-0.5 bg-orange-200"></div>
-        <NavLink to="/login" className="relative">
-          {/* <span className="absolute -top-2 -right-2 bg-orange-600 text-white text-[10px] w-5 h-5 rounded-full flex items-center justify-center border-2 border-white">2</span> */}
-          <RiUser5Line className="h-8 w-8 text-orange-600" />
-        </NavLink>
-        <button onClick={confirmLogout} className="relative text-orange-600 text-sm font-bold uppercase tracking-wider">
-          {/* <span className="absolute -top-2 -right-2 bg-orange-600 text-white text-[10px] w-5 h-5 rounded-full flex items-center justify-center border-2 border-white">2</span> */}
-          Logout
-        </button>
+
+        <div className={`${ !menuOpen ? 'hidden' : 'block'} md:flex`}>
+          <NavLink to="/login" className="relative">
+            {/* <span className="absolute -top-2 -right-2 bg-orange-600 text-white text-[10px] w-5 h-5 rounded-full flex items-center justify-center border-2 border-white">2</span> */}
+            <RiUser5Line className="h-8 w-8 text-orange-600" />
+          </NavLink>
+          <button onClick={confirmLogout} className="relative text-orange-600 text-sm font-bold uppercase tracking-wider">
+            {/* <span className="absolute -top-2 -right-2 bg-orange-600 text-white text-[10px] w-5 h-5 rounded-full flex items-center justify-center border-2 border-white">2</span> */}
+            <RiLoginBoxFill className="h-8 w-8 text-orange-600" />
+          </button>
+        </div>
+        {/* Hamburger menu untuk tampilan mobile */}
+        <div className="md:hidden flex items-center">
+          <button onClick={() => setMenuOpen(!menuOpen)}>
+            <RiMenu3Line className="h-8 w-8 text-orange-600" />
+          </button>
+        </div>
       </nav>
     </div>
   );
